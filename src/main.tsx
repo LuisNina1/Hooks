75d11f50e8982a4674ae5020a6e09aeb6da22e37
luis <luisninaj@outlook.com>
2026-01-02 19:31:08 -0500

    Se agreago TaksApp

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import { HooksApp } from './HooksApp.tsx'
 import './index.css'
-import { PokemonPage } from './03-examples/PokemonPage.tsx'
-import { FocusScreen } from './04-useRef/FocusScreen.tsx'
+import { TasksApp } from './05-useReducer/TaskApp.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FocusScreen></FocusScreen>
+    <TasksApp></TasksApp>
   </StrictMode>,
 )
